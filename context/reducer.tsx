90216037f2409d63cca88d83ae42a9028d338c0e
Jose <38558352+joseokwu@users.noreply.github.com>
2022-10-21 08:10:31 +0100

    updates

--- context/reducer.tsx
+++ context/reducer.tsx
@@ -1,26 +1,23 @@
 import { DISPLAY_ALERT, CLEAR_ALERT, TOGGLE_LOAD } from './actions';
 import { initialState } from './appContext';
 
-const reducer = (state, action) => {
-  if (action.type === DISPLAY_ALERT) {
-    const { showAlert, alertText, alertType } = action.payload;
-    return {
-      ...state,
-      showAlert: showAlert,
-      alertText: alertText,
-      alertType: alertType,
-    };
-  }
-
-  if (action.type === CLEAR_ALERT) {
-    return { ...state, showAlert: false, alertText: '', alertType: '' };
-  }
-
-  if (action.type === TOGGLE_LOAD) {
-    return { ...state, isLoading: !state.isLoading };
-  }
-
-  throw new Error(`no such action: ${action.type}`);
+const reducer = (state: {}, action: {}) => {
+  // if (action.type === DISPLAY_ALERT) {
+  //   const { showAlert, alertText, alertType } = action.payload;
+  //   return {
+  //     ...state,
+  //     showAlert: showAlert,
+  //     alertText: alertText,
+  //     alertType: alertType,
+  //   };
+  // }
+  // if (action.type === CLEAR_ALERT) {
+  //   return { ...state, showAlert: false, alertText: '', alertType: '' };
+  // }
+  // if (action.type === TOGGLE_LOAD) {
+  //   return { ...state, isLoading: !state.isLoading };
+  // }
+  // throw new Error(`no such action: ${action.type}`);
 };
 
 export default reducer;
